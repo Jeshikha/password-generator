fb97cc1723c4617659ce4d36923515e97a4eda79
Jeshikha <138247355+Jeshikha@users.noreply.github.com>
2023-08-24 13:22:26 +0200

    addind explantions of chosen functions for more understanding

--- script.js
+++ script.js
@@ -89,24 +89,34 @@ var upperCasedCharacters = [
 ];
 
 // Function to prompt user for password options to get password criteria from user
+
+// Defining a function named getPasswordOptions
 function getPasswordOptions() {
+  // Prompting the user to enter the desired password length and stores it in the length variable 
+  // after converting it to an integer using parseInt()
   var length = parseInt(prompt("Enter the desired password length (between 8 and 128):"));
 
+  // Checking if the entered password length is not a valid number, or if it's less than 8 or greater than 128. 
+  // If any of these conditions are met, an alert is shown, and the function returns early.
   if (isNaN(length) || length < 8 || length > 128) {
     alert("Please enter a valid password length.");
     return;
   }
-
+  // Gathering Character Type Preferences by using the confirm() function to interact with the user 
+  // and gather preferences for including special, numeric, lowercase, and uppercase characters in the password.
   var hasSpecialCharacters = confirm("Would you like to include special characters?");
   var hasNumericCharacters = confirm("Would you like to include numeric characters?");
   var hasLowercaseCharacters = confirm("Would you like to include lowercase characters?");
   var hasUppercaseCharacters = confirm("Would you like to include uppercase characters?");
 
+  // Validating Character Type Selection by checking if none of the character types have been selected. 
+  // If none are selected, an alert is shown, and the function returns early.
   if (!hasSpecialCharacters && !hasNumericCharacters && !hasLowercaseCharacters && !hasUppercaseCharacters) {
     alert("At least one character type should be selected.");
     return;
   }
-
+  // Returning Password Options the reason why it is key to contruct and return an object containing the gathered options: 
+  // password length and the selected character types.
   return {
     length: length,
     hasSpecialCharacters: hasSpecialCharacters,
